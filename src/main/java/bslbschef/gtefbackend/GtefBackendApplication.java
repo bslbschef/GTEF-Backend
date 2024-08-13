2ce2b9b384fd64aa20635155496fcbfb573fc89a
bslbschef <bslbschef@gmail.com>
2024-08-13 09:26:34 +0800

    'new'

---
+++ src/main/java/bslbschef/gtefbackend/GtefBackendApplication.java
@@ -0,0 +1,13 @@
+package bslbschef.gtefbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtefBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtefBackendApplication.class, args);
+    }
+
+}
